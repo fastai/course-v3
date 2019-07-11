a2b5b1c5420fe24867b06bab796ea70575e0673f
Aliakbar <34204311+Salehbigdeli@users.noreply.github.com>
2019-07-11 15:11:56 +0430

    Update notebook2script.py
    fix: when "exp" directory doesn't exists the script makes the directory.

--- nbs/dl2/notebook2script.py
+++ nbs/dl2/notebook2script.py
@@ -65,6 +65,7 @@ def notebook2scriptSingle(fname):
     for cell in code_cells: module += ''.join(cell['source'][1:]) + '\n\n'
     # remove trailing spaces
     module = re.sub(r' +$', '', module, flags=re.MULTILINE)
+    if not (fname.parent/'exp').exists(): (fname.parent/'exp').mkdir()
     output_path = fname.parent/'exp'/fname_out
     open(output_path,'w').write(module[:-2])
     print(f"Converted {fname} to {output_path}")
