45445362d48b101f0a2ea36c3fe50f7ad4182895
Jeremy Howard <j@fast.ai>
2020-07-24 15:46:39 -0700

    new js build

---
+++ docs/videos/precache-manifest.060032dc9d85b4be8e6eadce32aad4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d1b9198f13e1210dcc2",
+    "url": "/static/css/main.8e8ebadb.chunk.css"
+  },
+  {
+    "revision": "6d1b9198f13e1210dcc2",
+    "url": "/static/js/main.98ade38b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9794c14f770447f0e28b",
+    "url": "/static/css/2.4a1df0d5.chunk.css"
+  },
+  {
+    "revision": "9794c14f770447f0e28b",
+    "url": "/static/js/2.f6c3b6e0.chunk.js"
+  },
+  {
+    "revision": "d2b5a105437384859127f62b333e455b",
+    "url": "/index.html"
+  }
+];
